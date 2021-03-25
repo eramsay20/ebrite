2e308806b7ee8e88fb454f42c3e260de53a2ee7b
Eric Ramsay <eramsay10@gmail.com>
2021-03-25 08:31:56 -0400

    added express dependencies; added middleware for logging and security

---
+++ authenticate-me/backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan'); // logs info about server req/res
+const cors = require('cors'); // cross origin resource sharing mw
+const csurf = require('csurf'); // CSRF protection mw
+const cookieParser = require('cookie-parser'); // parses cookies (for csrf)
+const helmet = require('helmet'); // security mw
+
+const { environment } = require('./config/index'); // get environment
+const isProduction = environment === 'production'; // boolean checking whether being run in dev or prod
+
+const app = express() // init Express application
+app.use(morgan('dev')) // connect morgan mw to log info about req/res
+app.use(cookieParser()); // add CP mw to parse cookies
+app.use(express.json()); // add express.json mw to parse JSON bodies from app/json requests
+
+// Security Middleware --------------
+
+// enable cors only in dev; CORS not a problem in React prod env
+if(isProduction) app.use(cors())
+
+// helmet helps set a variety of headers to better secure the app
+app.use(helmet({ contentSecurityPolicy: false }))
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && 'Lax',
+      httpOnly: true,
+    }
+  })
+)
+// The csurf middleware will add a _csrf cookie that is HTTP-only (can't be read by JavaScript) to any server response. It also adds a method on all requests (req.csrfToken) that will be set to another cookie (XSRF-TOKEN) later on. These two cookies work together to provide CSRF (Cross-Site Request Forgery) protection for your application.
