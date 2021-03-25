63915cc6093675116952f9a12580f1f6d68b53ac
Eric Ramsay <eramsay10@gmail.com>
2021-03-25 12:56:38 -0400

    handleValidationErrors middleware for login completed

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator'); // don't need validationResult since we're using custom handler instead
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
@@ -17,10 +19,19 @@ router.get('/', restoreUser, (req, res) => {
   }
 })
 
-
+// Login validation middleware functions; stored sequentially in an array
+const validateLogin = [
+  check('credential')
+    .exists({checkFalsy: true})
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({checkFalsy: true})
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+]
 
 // Log In -- TESTED, OK
-router.post('/', asyncHandler(async (req, res, next) => {
+router.post('/', validateLogin, asyncHandler(async (req, res, next) => {
   const { credential, password } = req.body;
 
   const user = await User.login({ credential, password });
--- authenticate-me/backend/utils/validation.js
+++ authenticate-me/backend/utils/validation.js
@@ -8,12 +8,12 @@ const handleValidationErrors = (req, _res, next) => {
 
   if(!validationErrors.isEmpty()){
     // map over errors and extract the err msg from each; store in array of err msgs
-    const errors = validationErrors.array().map(error => `${error.msg}`)
+    const errors = validationErrors.array().map(error => `${error.msg}`);
 
     const err = Error('Bad request.');
     err.errors = errors; // store msgs on new err obj
     err.status = 400;
-    error.title = 'Bad request.'
+    err.title = 'Bad request.'
     next(err);
   }
   next();
