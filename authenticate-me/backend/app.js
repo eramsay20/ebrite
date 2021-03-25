f0371e9719db97537ebc3a9bd99a79bf2c97fd47
Eric Ramsay <eramsay10@gmail.com>
2021-03-25 08:45:54 -0400

    app.js config and route setup complete

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -5,30 +5,35 @@ const csurf = require('csurf'); // CSRF protection mw
 const cookieParser = require('cookie-parser'); // parses cookies (for csrf)
 const helmet = require('helmet'); // security mw
 
+const routes = require('./routes/index')
 const { environment } = require('./config/index'); // get environment
 const isProduction = environment === 'production'; // boolean checking whether being run in dev or prod
 
 const app = express() // init Express application
-app.use(morgan('dev')) // connect morgan mw to log info about req/res
-app.use(cookieParser()); // add CP mw to parse cookies
-app.use(express.json()); // add express.json mw to parse JSON bodies from app/json requests
 
-// Security Middleware --------------
+  app.use(morgan('dev')) // connect morgan mw to log info about req/res
+  app.use(cookieParser()); // add CP mw to parse cookies
+  app.use(express.json()); // add express.json mw to parse JSON bodies from app/json requests
 
-// enable cors only in dev; CORS not a problem in React prod env
-if(isProduction) app.use(cors())
+// --------------- Security Middleware -------------- //
+  // enable cors only in dev; CORS not a problem in React prod env
+  if(isProduction) app.use(cors())
 
-// helmet helps set a variety of headers to better secure the app
-app.use(helmet({ contentSecurityPolicy: false }))
+  // helmet helps set a variety of headers to better secure the app
+  app.use(helmet({ contentSecurityPolicy: false }))
 
-// Set the _csrf token and create req.csrfToken method
-app.use(
-  csurf({
-    cookie: {
-      secure: isProduction,
-      sameSite: isProduction && 'Lax',
-      httpOnly: true,
-    }
-  })
-)
-// The csurf middleware will add a _csrf cookie that is HTTP-only (can't be read by JavaScript) to any server response. It also adds a method on all requests (req.csrfToken) that will be set to another cookie (XSRF-TOKEN) later on. These two cookies work together to provide CSRF (Cross-Site Request Forgery) protection for your application.
+  // Set the _csrf token and create req.csrfToken method. The csurf middleware will add a _csrf cookie that is HTTP-only (can't be read by JavaScript) to any server response. It also adds a method on all requests (req.csrfToken) that will be set to another cookie (XSRF-TOKEN) later on. These two cookies work together to provide CSRF (Cross-Site Request Forgery) protection for your application.
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && 'Lax',
+        httpOnly: true,
+      }
+    })
+  )
+// ----------------------------------------------------- //
+
+  app.use(routes) // connect app to routes, with all other middleware in place
+
+module.exports = app;
