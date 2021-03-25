0e728cf78f0325c3594d6a3399c583ae98eda5d1
Eric Ramsay <eramsay10@gmail.com>
2021-03-25 09:14:26 -0400

    added error handlers and formatting mw to app.js

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors'); // cross origin resource sharing mw
 const csurf = require('csurf'); // CSRF protection mw
 const cookieParser = require('cookie-parser'); // parses cookies (for csrf)
 const helmet = require('helmet'); // security mw
+const { ValidationError } = require('sequelize');
 
 const routes = require('./routes/index')
 const { environment } = require('./config/index'); // get environment
@@ -15,7 +16,8 @@ const app = express() // init Express application
   app.use(cookieParser()); // add CP mw to parse cookies
   app.use(express.json()); // add express.json mw to parse JSON bodies from app/json requests
 
-// --------------- Security Middleware -------------- //
+// ---- Security Middleware ------ //
+
   // enable cors only in dev; CORS not a problem in React prod env
   if(isProduction) app.use(cors())
 
@@ -32,8 +34,42 @@ const app = express() // init Express application
       }
     })
   )
-// ----------------------------------------------------- //
+// ------------------------------------
 
   app.use(routes) // connect app to routes, with all other middleware in place
+
+// ------- Error handlers -------------
+
+  // Catch unhandled requests and forward to error handler.
+  app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+  // Process sequelize errors
+  app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+  });
+
+  // Error formatter
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack,
+    });
+  });
+
 
 module.exports = app;
