b5b73d9531ac1ba4d2b3267373bd4f2ae2846ea9
Eric Ramsay <eramsay10@gmail.com>
2021-03-30 15:07:27 -0400

    hotfix for footer links

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 function Footer(){
   const WIKI_PATH='https://github.com/eramsay20/ebrite/wiki'
@@ -8,8 +7,8 @@ function Footer(){
   return (
     <nav>
       <div>
-        <NavLink to={GITHUB_PROFILE_PATH} style={{ marginLeft: '.5rem' }}>Creator Github Profile</NavLink>
-        <NavLink to={WIKI_PATH} style={{ marginLeft: '.5rem' }}>e.brite Wiki</NavLink>
+        <a href={GITHUB_PROFILE_PATH} style={{ marginLeft: '.5rem' }}>Github Profile</a>
+        <a href={WIKI_PATH} style={{ marginLeft: '.5rem' }}>e.brite Wiki</a>
       </div>
     </nav>
   );
