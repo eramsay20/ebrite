06002d1bddecd4455f2c6c0f9d593d4b3516f057
Eric Ramsay <eramsay10@gmail.com>
2021-04-01 09:12:14 -0400

    fixed nav bar search bar, removed navbarprofile drop down and replaced with navlinks

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import LoginFormModal from '../LoginFormModal'
-import ProfileButton from './ProfileButton';
+import * as sessionActions from '../../store/session';
 
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
 
-  let sessionLinks; // declare variable to store which links to render
+  // declare variable to store which links to render
+  let sessionLinks;
+
+  // log out button functionality
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
 
   if (sessionUser) { // if logged in, only show Profile button
     sessionLinks = (
-      <ProfileButton user={sessionUser} />
+      <>
+       <NavLink className="nav-link" to="#" style={{ marginLeft: '.5rem' }}>Profile</NavLink>
+       <NavLink onClick={logout} className="nav-link" to="#" style={{ marginLeft: '.5rem' }}>Log Out</NavLink>
+      </>
     );
   } else { // else, render login & signup links instead
     sessionLinks = (
@@ -25,12 +36,15 @@ function Navigation({ isLoaded }){
   }
 
   return (
-    <>
-        <NavLink exact to="/">
-          <img style={{width: '70px'}} src="https://github.com/eramsay20/ebrite/blob/master/wiki-resources/ebrite_logo_trans.png?raw=true"></img>
-        </NavLink>
-        <div className={`search-bar`}>SEARCH BAR HERE</div>
-        {isLoaded && sessionLinks}
+    <>  <div style={{display: 'flex', flexDirection: 'flex-start', alignItems: 'center', justifyContent: 'flex-start'}}>
+          <NavLink style={{marginRight:'30px'}}exact to="/">
+            <img style={{width: '70px'}} src="https://github.com/eramsay20/ebrite/blob/master/wiki-resources/ebrite_logo_trans.png?raw=true"></img>
+          </NavLink>
+          <div className={`search-bar`}>Search events</div>
+        </div>
+        <div style={{display: 'flex', flexDirection: 'flex-end', alignItems: 'center'}}>
+          {isLoaded && sessionLinks}
+        </div>
     </>
   );
 }
