39bfaad5882c3b660d11d6453de78bf00156da33
Eric Ramsay <eramsay10@gmail.com>
2021-04-02 20:04:19 -0400

    updated datetime formmatting;

--- frontend/src/components/Utils/index.js
+++ frontend/src/components/Utils/index.js
@@ -1,8 +1,14 @@
 
 // date formatting function
 export const dateFormat = (dateString) => {
-    const dateObj = new Date(dateString)
-    const day = dateObj.toLocaleDateString();
-    const time = dateObj.toLocaleTimeString()
-    return `${day}, ${time}`
+    const date = new Date(dateString)
+    const day = date.toDateString();
+    const weekday = day.split(" ")[0]
+    const month = day.split(" ")[1]
+    const dateNum = day.split(" ")[2]
+
+    const time = date.toLocaleTimeString()
+    const hour = time.split(':')[0]
+    const amPm = time.split(' ')[1]
+    return `${weekday}, ${month} ${dateNum} @ ${hour}:00 ${amPm}`
   }
