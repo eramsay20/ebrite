ecfab4b8d56e4ab89306ff88e7213a860f92d0ff
eramsay20 <eramsay10@gmail.com>
2021-05-26 19:13:51 -0400

    updated css for event cards, banner and fav icons

--- frontend/src/components/MainPageBanner/index.js
+++ frontend/src/components/MainPageBanner/index.js
@@ -5,7 +5,7 @@ function MainPageBanner(){
   const banner = require('../../front-assets/banner1.png')
   return (
       <div className='main-page-banner'>
-        <img alt={'banner'} src={banner}></img>
+      <img alt={'banner'} src={'https://github.com/eramsay20/ebrite/blob/master/wiki-resources/eBrite-banner-1.png?raw=true'}></img>
       </div>
   );
 }
